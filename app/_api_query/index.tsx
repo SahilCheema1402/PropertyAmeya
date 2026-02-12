91813ad6666dc23bff842be62406255cd8559e8e
Sahil <sahil.cheema@iameya.in>
2026-02-13 00:55:56 +0530

    index.ts

--- app/_api_query/index.tsx
+++ app/_api_query/index.tsx
@@ -1,4 +1,4 @@
 // export const baseUrl = "https://property360.iameya.in/api/v1"
-// export const baseUrl = "https://property360.iameya.com/api/v1"
-export const baseUrl = "http://localhost:3000/api/v1"
+export const baseUrl = "https://property-ameya.vercel.app/api/v1"
+// export const baseUrl = "http://localhost:3000/api/v1"
 
